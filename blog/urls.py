3a159995cdf132be6af8dde40e9867c941c55b5a
Angad Singh Sandhu <angad277@gmail.com>
2020-05-15 20:20:13 +0530

    blog functioning and polish

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('',views.allblogs, name = 'allblogs'),
+    path('<int:blog_id>/', views.detail,name='detail'),
+    
+    
+]
